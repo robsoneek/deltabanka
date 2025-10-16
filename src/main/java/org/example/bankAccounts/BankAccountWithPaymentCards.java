e829f012014429e7eaac05afd01acc19212d33a5
RObinEk <robin@neserposse.cz>
2025-10-16 10:54:32 +0200

    payment card

--- src/main/java/org/example/bankAccounts/BankAccountWithPaymentCards.java
+++ src/main/java/org/example/bankAccounts/BankAccountWithPaymentCards.java
@@ -11,24 +11,19 @@ import java.util.Map;
 
 public class BankAccountWithPaymentCards extends BaseBankAccount{
     private List<PaymentCard> paymentCards;
-    public Map<String, PaymentCard> paymentCardsMap;
 
     public BankAccountWithPaymentCards(String uuid, String bankAccountNumber, Customer customer, double balance){
         super(uuid, bankAccountNumber, customer, balance);
 
         this.paymentCards = new ArrayList<>();
-        this.paymentCardsMap = new HashMap<>();
     }
 
     public void addPaymentCard(PaymentCard paymentCard){
-        this.paymentCards.add(paymentCard);
-        this.paymentCardsMap.put(paymentCard.getCardNumber(), paymentCard);
         paymentCard.linkToBankAccount(this);
     }
 
     public void removePaymentCard(PaymentCard paymentCard) {
         this.paymentCards.remove(paymentCard);
-        this.paymentCardsMap.remove(paymentCard.getCardNumber());
         paymentCard.linkToBankAccount(null);
     }
 
